bcf9cc22b8b7782201bdb447d1aec786161c6279
shekhar kumar <40865395+shekharkumar1000@users.noreply.github.com>
2021-06-12 23:56:08 +0530

    initial commit

---
+++ wp-content/themes/startinger/js/search-top9f31.js
@@ -0,0 +1,31 @@
+function startingerSearch() {
+	var x = document.getElementById("big-search");
+	if (x.style.display == "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function startingerCloseSearch () {
+	var close = document.getElementById("big-search"); 
+	  if (close.style.display == "block") {
+		close.style.display = "none";
+	} 
+}
+
+function startingerSearchMobile() {
+	var x = document.getElementById("big-search-mobile");
+	if (x.style.display == "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function startingerCloseSearchMobile () {
+	var close = document.getElementById("big-search-mobile"); 
+	  if (close.style.display == "block") {
+		close.style.display = "none";
+	} 
+}
